114f69dffe82fe328abdd892253c5d3700f4e401
AshishOliverSamuel <imashishosamuel@gmail.com>
2026-09-22 23:34:13 +0530

    Add project files for container scanning pipeline

---
+++ app.py
@@ -0,0 +1,10 @@
+from flash import Flask
+
+app = Flsk(--name--)
+
+@app.route("/")
+def home():
+return "Container Scanning Pipeline Active!"
+
+if --name-- == "--main--":
+app.run(host="0.0.0.0", port=5000)
